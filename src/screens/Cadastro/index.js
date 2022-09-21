4173d2a51ff038cee4fb83b0b8a5d62d51109bde
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-09-21 11:45:52 -0300

    Add files via upload

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, TextInput, View, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
 import styles from './style.js'
 import Header from '../../Components/Header/index.js';
+import Axios from 'axios'
+
 
 function Cadastro({ navigation }) {
 
+  const [email, setEmail] = useState('')
+  const [pswd, setPswd] = useState('')
+
   function back(){
     navigation.navigate("Login")
+  }
+
+  const Login = () => {
+    Axios.post('http://localhost:3000/loginUser', {
+      email : email,
+      pswd : pswd
+    }).then((response) => {
+      if (response.data === 1) {
+        navigation.navigate('Home')
+      }
+    })
+    .catch((err) => console.log(err))
   }
 
     return (
@@ -22,12 +39,14 @@ function Cadastro({ navigation }) {
             
             <View style={styles.viewCadastro}>
                 <Text style={styles.txtCad}>LOGIN</Text>
-                <TextInput style={styles.input} placeholder=' Usuário'/>
-                <TextInput style={styles.input} placeholder=' Senha' secureTextEntry={true}/>
+                <TextInput style={styles.input} placeholder=' Usuário' onChangeText={email => setEmail(email)} value={email}/>
+                <TextInput style={styles.input} placeholder=' Senha' secureTextEntry={true}
+                onChangeText={pswd => setPswd(pswd)} value = {pswd}
+                />
             </View>
 
             <View style={styles.viewCadastro}>
-              <TouchableOpacity style={styles.btn} title="Cadastro" onPress={() => navigation.navigate('Home')}>
+              <TouchableOpacity style={styles.btn} title="Cadastro" onPress={Login}>
                   <Text style={styles.txtBtn}>Entrar</Text>
               </TouchableOpacity>
             </View>
